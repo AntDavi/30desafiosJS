b004e4c76eab9e3a5c0aee10d9e0c0a2f9ea2d0e
Anthony Davi <ant.davi07@gmail.com>
2023-05-12 20:59:05 -0300

    dia 01

--- #01/app.js
+++ #01/app.js
@@ -1,3 +1,20 @@
+// 1. Ajudando a Academia
+// A Academia de Oficiais da Federação precisa da sua ajuda
+// para criar um programa que automatize os cálculos das
+// médias de alunos e turmas para melhor acompanhar os
+// seus desempenhos.
+// Este programa será incorporado em seu sistema principal,
+// portanto você precisa apenas desenvolver a função que
+// calcula a média.
+// No entanto, a função deve ser capaz de funcionar com
+// qualquer número de alunos ou turmas.
+
+// Desafio 1: Ajudando a Academia
+// Escreva uma função que recebe um número qualquer de
+// números inteiros como argumentos e retorne a média
+// aritmética entre eles.
+
+
 function media(notas) {
     let soma = 0
 
---
+++ #02/app.js
@@ -0,0 +1,14 @@
+// 2. Procedimento Recursivo I
+// A equipe de manutenção da nave Highwind solicitou a sua
+// ajuda para analisar o desempenho do computador portátil
+// utilizado pela equipe de reconhecimento.
+// Para isso, em um determinado momento, você precisou
+// simular a criação de blocos de informação em formato de
+// texto recursivamente, então decidiu criar uma função para
+// imitar esse comportamento.
+
+// Desafio 2: Procedimento ReCUfSiVO I
+// Escreva uma função que recebe um número e retorna uma
+// quantidade equivalente de "chunks" separados por um traço
+// sem utilizar nenhuma estrutura de repetição (while,
+// dowhile, for, etc).
