7f3ed65fa700335ed554f38d83bf4c7cd648fdd5
Anthony Davi <ant.davi07@gmail.com>
2023-05-13 10:31:56 -0300

    desafio 02

--- #01/app.js
+++ #01/app.js
@@ -1,30 +1,27 @@
 // 1. Ajudando a Academia
-// A Academia de Oficiais da Federação precisa da sua ajuda
-// para criar um programa que automatize os cálculos das
-// médias de alunos e turmas para melhor acompanhar os
-// seus desempenhos.
-// Este programa será incorporado em seu sistema principal,
-// portanto você precisa apenas desenvolver a função que
-// calcula a média.
-// No entanto, a função deve ser capaz de funcionar com
-// qualquer número de alunos ou turmas.
+// A Academia de Oficiais da Federação precisa da sua ajuda para criar um programa que automatize os cálculos das médias de alunos e turmas para melhor acompanhar os seus desempenhos.
+// Este programa será incorporado em seu sistema principal, portanto você precisa apenas desenvolver a função que calcula a média. No entanto, a função deve ser capaz de funcionar com qualquer número de alunos ou turmas.
 
 // Desafio 1: Ajudando a Academia
-// Escreva uma função que recebe um número qualquer de
-// números inteiros como argumentos e retorne a média
-// aritmética entre eles.
+// Escreva uma função que recebe um número qualquer de números inteiros como argumentos e retorne a média aritmética entre eles.
 
 
 function media(notas) {
-    let soma = 0
+    // Inicializa a variável soma com o valor zero
+    let soma = 0;
 
+    // Itera sobre cada nota no array de notas usando forEach
     notas.forEach(nota => {
-        soma += nota
+        // Adiciona a nota atual à soma total
+        soma += nota;
     });
 
-    const media = soma / notas.length
+    // Divide a soma pelo número de notas para calcular a média
+    const media = soma / notas.length;
 
-    return media
+    // Retorna a média das notas
+    return media;
 }
 
-console.log(media([8, 10, 5, 8]))
+// Chama a função media com um array de notas como argumento e imprime o resultado no console
+console.log(media([8, 10, 5, 8]));
--- #02/app.js
+++ #02/app.js
@@ -1,14 +1,35 @@
 // 2. Procedimento Recursivo I
-// A equipe de manutenção da nave Highwind solicitou a sua
-// ajuda para analisar o desempenho do computador portátil
-// utilizado pela equipe de reconhecimento.
-// Para isso, em um determinado momento, você precisou
-// simular a criação de blocos de informação em formato de
-// texto recursivamente, então decidiu criar uma função para
-// imitar esse comportamento.
+// A equipe de manutenção da nave Highwind solicitou a sua ajuda para analisar o desempenho do computador portátil utilizado pela equipe de reconhecimento.
+// Para isso, em um determinado momento, você precisou simular a criação de blocos de informação em formato de texto recursivamente, então decidiu criar uma função para imitar esse comportamento.
 
 // Desafio 2: Procedimento ReCUfSiVO I
-// Escreva uma função que recebe um número e retorna uma
-// quantidade equivalente de "chunks" separados por um traço
-// sem utilizar nenhuma estrutura de repetição (while,
-// dowhile, for, etc).
+// Escreva uma função que recebe um número e retorna uma quantidade equivalente de "chunks" separados por um traço sem utilizar nenhuma estrutura de repetição (while, dowhile, for, etc).
+
+function separarChunks(numero) {
+    // converter o número para uma string
+    const numeroString = numero.toString();
+
+    // dividir a string em um array de caracteres
+    const numeroArray = numeroString.split('');
+
+    // usar o método reduce para criar um novo array com os "chunks" separados por um traço
+    const chunksArray = numeroArray.reduce((chunks, numero, index) => {
+        // adicionar o número atual ao chunk atual
+        chunks[chunks.length - 1] += numero;
+
+        // adicionar um novo chunk se o índice atual não for divisível por 3
+        if ((index + 1) % 3 === 0 && index !== numeroArray.length - 1) {
+            chunks.push('');
+        }
+
+        return chunks;
+    }, ['']);
+
+    // juntar os "chunks" usando o método join
+    const chunksString = chunksArray.join('-');
+
+    // retornar a string final com os "chunks" separados por traço
+    return chunksString;
+}
+
+console.log(separarChunks(123456789)); // Output: "123-456-789"
